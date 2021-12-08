964b1d63d581e27fb2f4736cd5c0fe4babed0c7c
dovanan95 <dovanan95@gmail.com>
2021-12-08 17:55:47 +0900

    a

--- app.js
+++ app.js
@@ -144,6 +144,11 @@ app.get('/home', function(req, res){
     res.render('./views/home');
 })
 
+app.get('/searchUserByID', function(req, res){
+    console.log(req.query.id);
+    res.send(JSON.stringify({'data': req.query.id}));
+})
+
 server.listen(8082, () => {
     console.log('Server đang chay tren cong 8082');
  });
