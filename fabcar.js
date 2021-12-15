44e37fd4856c460b8a5220edffef7135e1e122cf
dovanan95 <dovanan95@gmail.com>
2021-12-16 03:51:05 +0900

    query mess

--- fabcar.js
+++ fabcar.js
@@ -526,7 +526,7 @@ class FabCar extends Contract {
                         {"sender": receiver, "receiver": sender, "timestamp":{"$gte": 0}}
                     ]
                 },
-                "sort":[{"timestamp":"desc"}],
+                //"sort":[{"timestamp":"desc"}],
                 "limit": 100,
                 "skip":0
             }
