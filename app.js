2e9a9ac7b836d4eafcf4300c798314e13ca8c0cc
dovanan95 <dovanan95@gmail.com>
2021-12-17 02:01:51 +0900

    build net sh

--- app.js
+++ app.js
@@ -12,7 +12,31 @@ const { Gateway, Wallets } = require('fabric-network');
 const fs = require('fs');
 const path = require('path');
 
+const ccpPath = path.resolve(__dirname, '..', '..', 'test-network', 'organizations', 'peerOrganizations', 'org1.example.com', 'connection-org1.json');
+let ccp = JSON.parse(fs.readFileSync(ccpPath, 'utf8'));
 
+        // Create a new file system based wallet for managing identities.
+const walletPath = path.join(process.cwd(), 'wallet');
+const wallet = await Wallets.newFileSystemWallet(walletPath);
+console.log(`Wallet path: ${walletPath}`);
+
+        // Check to see if we've already enrolled the user.
+const identity = await wallet.get('appUser');
+if (!identity) {
+    console.log('An identity for the user "appUser" does not exist in the wallet');
+    console.log('Run the registerUser.js application before retrying');
+    return;
+}
+
+        // Create a new gateway for connecting to our peer node.
+const gateway = new Gateway();
+await gateway.connect(ccp, { wallet, identity: 'appUser', discovery: { enabled: true, asLocalhost: true } });
+
+        // Get the network (channel) our contract is deployed to.
+const network = await gateway.getNetwork('mychannel');
+
+        // Get the contract from the network.
+const contract = network.getContract('fabcar');
 const socketIo = require("socket.io")(server, {
    cors: {
        origin: "*",
@@ -150,6 +174,7 @@ app.get('/chat', function(req, res){
 
 app.post('/load_chat_history', function(req, res){
     console.log(req.body.id);
+    const chat_data = contract.evaluateTransaction('queryMessage');
     res.send(JSON.stringify(sample_chat_data));
 })
 
--- build_n.sh
+++ build_n.sh
@@ -9,3 +9,6 @@ cd addOrg3/
 ./addOrg3.sh up -c mychannel -s couchdb
 cd ..
 ./network.sh deployCC -ccn fabcar -ccv 1 -cci initLedger -ccl javascript  -ccp ../chaincode/fabcar/javascript/
+cd ../fabcar/javascript/
+node enrollAdmin.js
+node registerUser.js
