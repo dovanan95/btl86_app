b3e2ce967b8103c52a01e7b856e70c0315e69b0a
dovanan95 <dovanan95@gmail.com>
2022-04-06 02:09:10 +0900

    db_mssql

---
+++ db_mssql.sql
@@ -0,0 +1,13 @@
+create database httcddh2018_86_130
+go
+create table DoIT_CanBo 
+(Id INT NOT NULL PRIMARY KEY,
+Id_DNN INT,
+FirstName NVARCHAR(50),
+LastName NVARCHAR(50),
+TenDayDu NVARCHAR(250),
+TenDangNhap NVARCHAR(100),
+MatKhau NVARCHAR(255),
+Mobile NVARCHAR(50)
+)
+go
